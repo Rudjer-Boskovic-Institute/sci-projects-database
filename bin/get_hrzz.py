3a3c5a62cfb8927042006b35a50336c0b44e8c7b
Alen Vodopijevec <alen@irb.hr>
2015-01-22 17:04:06 +0100

    dev

--- bin/get_hrzz.py
+++ bin/get_hrzz.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
+import pprint
 import mechanize
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 br = mechanize.Browser()
 #br.set_all_readonly(False)
@@ -25,7 +26,7 @@ response = br.open(url)
 
 link1 = br.find_link(url_regex='page',nr=1)
 
-print link1.url
+print (link1.url)
 
 response = br.open(urlhost + '/' + link1.url)
 
@@ -39,13 +40,52 @@ html_doc = response.read()
 
 soup = BeautifulSoup(html_doc)
 
-print soup.getText()
+ptitle = soup.select('.page_content > strong > span')
+project_title = ptitle[0].findAll(text=True)
+print str(project_title) + "\n"
 
-print soup.findAll("div", class_='page_content')
+phead = soup.select('.page_content > table:nth-of-type(1) > tr:nth-of-type(2) > td:nth-of-type(1)')
+project_head = phead[0].findAll(text=True)
+print str(project_head) + "\n"
+
+ptype = soup.select('.page_content > table:nth-of-type(1) > tr:nth-of-type(2) > td:nth-of-type(2)')
+project_type = ptype[0].findAll(text=True)
+print str(project_type) + "\n"
+
+pcall = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(1)')
+project_call = pcall[0].findAll(text=True)
+print str(project_call) + "\n"
+
+pcode = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(2)')
+project_code = pcode[0].findAll(text=True)
+print str(project_code) + "\n"
+
+pacro = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(3)')
+project_acro = pacro[0].findAll(text=True)
+print str(project_acro) + "\n"
+
+pdura = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(4)')
+project_dura = pdura[0].findAll(text=True)
+print str(project_dura) + "\n"
+
+pstatus = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(5)')
+project_status = pstatus[0].findAll(text=True)
+print str(project_status) + "\n"
+
+pval = soup.select('.page_content > table:nth-of-type(2) > tr:nth-of-type(2) > td:nth-of-type(6)')
+project_val = pval[0].findAll(text=True)
+print str(project_val) + "\n"
 
 
 
+pdet = soup.select('.page_content')
+project_det = pdet[0].findAll(text=True)
 
+
+#output = []
+#output += [project_det[3], project_det[6], project_det[7], project_det[14], project_det[15], project_det[16], project_det[17], project_det[18]]  
+
+#pprint.pprint(output)
 
 #################################################################################
 
@@ -68,4 +108,27 @@ print soup.findAll("div", class_='page_content')
 
 
 # response = br.open(url)
-# print response.read() 
+# print response.read()
+
+# SOUP
+
+#soup = BeautifulSoup(html_doc)
+
+#print soup.getText()
+
+
+
+# SELECTORS
+
+#pprint(soup.select(".sister")) # select by class
+#pprint(soup.select("#link1")) # select by id
+#pprint(soup.select('a[href="http://example.com/elsie"]')) 
+# find tags by attribute value
+#pprint(soup.select('a[href^="http://example.com/"]'))
+# find tags by attribute value, all contains 'http://example.com/'
+#pprint(soup.select('p[lang|=en]')) # Match language codes
+ 
+
+#print [elm.span.text for elm in soup.findAll('div', {'class': 'title'})]
+
+##
